a79ccbc93571b8a4ff8245a1a66828bf53e04219
Annuru-Kulasekhar <kulasekhar7799@gmail.com>
2026-04-29 21:58:32 +0530

    terraform

--- 30-rds/locals.tf
+++
@@ -1,9 +0,0 @@
-locals {
-    common_tags = {
-        Project = var.project
-        Environment = var.environment
-        Terraform = "true"
-    }
-    mysql_sg_id = data.aws_ssm_parameter.mysql_sg_id.value
-    database_subnet_group_name = data.aws_ssm_parameter.database_subnet_group_name.value
-}
